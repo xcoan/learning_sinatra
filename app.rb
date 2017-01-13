e4ce62763d5ee8ac045dde58e21bc754e3298e6d
X Coan <xjcoan@gmail.com>
2017-01-12 22:51:21 -0500

    Playing with CSS

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 class MySinatraApp < Sinatra::Base
+  # set :views, Proc.new    {File.join(root, "views/")}
+  # set :public_folder , Proc.new {File.join(root,"public")}
+  #
 
   configure :development do
     register Sinatra::Reloader
@@ -6,5 +9,9 @@ class MySinatraApp < Sinatra::Base
 
   get '/' do
     erb :index, locals: {now: Time.now, myname: 'X Coan'}
+  end
+
+  get '/song' do
+    erb :song
   end
 end
